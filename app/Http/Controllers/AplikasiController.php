ec8e48f997a7334ee418c4aee334844ad84c68a2
yusuf040700 <yusuf.marsudien10@gmail.com>
2021-10-28 16:09:26 +0700

    Adding inputBeasiswa1 method

--- app/Http/Controllers/AplikasiController.php
+++ app/Http/Controllers/AplikasiController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Beasiswa;
+use App\Models\Dosen;
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 
 class AplikasiController extends Controller
 {
-    //
+    public function all()
+    {
+        /**
+         * Shows all data from table Dosen and Mahasiswa
+         */
+        echo "## Dosen ## <br>";
+        $dosens = Dosen::all();
+        foreach ($dosens as $dosen) {
+            echo "$dosen->nama <br>";
+        }
+
+        echo "<hr>";
+        echo "## Mahasiswa ## <br>";
+        $mahasiswas = Mahasiswa::all();
+        foreach ($mahasiswas as $mahasiswa) {
+            echo "$mahasiswa->nama | $mahasiswa->jurusan <br>";
+        }
+    }
+
+    public function inputBeasiswa1()
+    {
+        $dosen = Dosen::where('nama', 'Lessie Hamill M.T')->first();
+
+        $beasiswa = new Beasiswa;
+        $beasiswa->nama = "Beasiswa Unggulan Dosen Indonesia";
+        $beasiswa->jumlah_dana = 50000000;
+
+        $dosen->beasiswa()->save($beasiswa);
+        echo "$dosen->nama dapat beasiswa $beasiswa->nama";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AplikasiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,22 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/all',
-[AplikasiController::class,'all']);
-Route::get('/input-beasiswa-1', [AplikasiController::class,'inputBeasiswa1']);
-Route::get('/input-beasiswa-2', [AplikasiController::class,'inputBeasiswa2']);
-Route::get('/tampil-beasiswa-1',[AplikasiController::class,'tampilBeasiswa1']);
-Route::get('/tampil-beasiswa-2',[AplikasiController::class,'tampilBeasiswa2']);
-Route::get('/tampil-beasiswa-3',[AplikasiController::class,'tampilBeasiswa3']);
-Route::get('/tampil-beasiswa-4',[AplikasiController::class,'tampilBeasiswa4']);
+Route::get('/all', [AplikasiController::class, 'all']);
+Route::get('/input-beasiswa-1', [AplikasiController::class, 'inputBeasiswa1']);
+Route::get('/input-beasiswa-2', [AplikasiController::class, 'inputBeasiswa2']);
+Route::get('/tampil-beasiswa-1', [AplikasiController::class, 'tampilBeasiswa1']);
+Route::get('/tampil-beasiswa-2', [AplikasiController::class, 'tampilBeasiswa2']);
+Route::get('/tampil-beasiswa-3', [AplikasiController::class, 'tampilBeasiswa3']);
+Route::get('/tampil-beasiswa-4', [AplikasiController::class, 'tampilBeasiswa4']);
 Route::get('/wherehasmorph-1', [AplikasiController::class, 'wherehasmorph2']);
-Route::get('/wherehasmorph-2',[AplikasiController::class,'wherehasmorph1']);
+Route::get('/wherehasmorph-2', [AplikasiController::class, 'wherehasmorph1']);
 
-Route::get('/update-beasiswa-1',[AplikasiController::class,'updateBeasiswa1']);
-Route::get('/update-beasiswa-2',[AplikasiController::class,'updateBeasiswa2']);
-Route::get('/delete',
-[AplikasiController::class,'delete']);
+Route::get('/update-beasiswa-1', [AplikasiController::class, 'updateBeasiswa1']);
+Route::get('/update-beasiswa-2', [AplikasiController::class, 'updateBeasiswa2']);
+Route::get('/delete', [AplikasiController::class, 'delete']);
